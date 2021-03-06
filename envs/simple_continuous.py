Changed default values, corrected linear conversion and return gradients from training

envs/simple_continuous.py
@@ -16,8 +16,8 @@ class BaseSimpleContinuous(object):
     Only one possible state. Different reward functions available.
     """
 
-    def __init__(self, max_reward: float = 1., min_reward: float = 0., min_action: float = 0.,
-                 max_action: float = 8., target_action: float = 4.5, reward_function: str = "linear"):
+    def __init__(self, max_reward: float = 1., min_reward: float = 0., min_action: float = -1.,
+                 max_action: float = 1., target_action: float = 0.5, reward_function: str = "linear"):
         """Creates a new simple continuous game
 
         Args:
@@ -158,8 +158,8 @@ class SimpleContinuousEnvironment(Environment):
 
 class BaseSimpleContinuousEnvironment(Environment):
 
-    def __init__(self, max_reward: float = 1., min_reward: float = 0., min_action: float = 0.,
-                 max_action: float = 8., target_action: float = 4.5, reward_function: str = "linear"):
+    def __init__(self, max_reward: float = 1., min_reward: float = 0., min_action: float = -1.,
+                 max_action: float = 1., target_action: float = 0.5, reward_function: str = "linear"):
 
         env = BaseSimpleContinuous(max_reward=max_reward,
                                    min_reward=min_reward,
@@ -193,8 +193,8 @@ class BaseSimpleContinuousEnvironment(Environment):
             next_environment_state (np.array), reward (float), terminated_environment (bool)
         """
 
-        # Action gets converted to possible values range
-        action = (action * (self.range_size / 2)) + (self.range_size / 2)
+        # Action gets converted to possible values range (simple linear conversion)
+        action = (((action - -1.) * self.range_size) / 2.) + self.env.min_action
         return self.env.step(action)
 
     def get_possible_states(self) -> np.array:
@@ -215,9 +215,9 @@ class BaseSimpleContinuousEnvironment(Environment):
 
 
 def main():
-    env = SimpleContinuousEnvironment()
+    env = BaseSimpleContinuousEnvironment()
     print(env.get_environment_state())
-    print(env.environment_step())
+    print(env.environment_step(np.array)(0))
 
 
 if __name__ == '__main__':

models/models.py
@@ -22,7 +22,7 @@ class SimpleModel(Model):
 
     def __init__(self, model_path: Path, layer_sizes: List[int], learning_rate: float,
                  actions_size: int, hidden_activation: str = "relu", mu_activation: str = "tanh",
-                 sigma_activation: str = "relu", target_action: float = 3.0,
+                 sigma_activation: str = "softplus", target_action: float = 3.0,
                  start_mu: float = 0.0, start_sigma: float = 1.0):
         """Creates a new FFNN model to represent a policy. Implements all needed
         methods from tf.keras.Model.
@@ -105,7 +105,7 @@ class SimpleModel(Model):
         gradients = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
 
-        return (mu, sigma), loss, log_probabilities
+        return (mu, sigma), loss, log_probabilities, gradients
 
     @tf.function
     def _get_log_probabilities(self, mu: tf.Tensor, sigma: tf.Tensor, actions: tf.Tensor) -> tf.Tensor:
@@ -169,12 +169,12 @@ def test():
     actions = model.produce_actions(state)
     print(f"actions train= {actions}")
 
-    (mu, sigma), loss, log_probabilities = model.train_step(state, actions, reward)
+    (mu, sigma), loss, log_probabilities, gradients = model.train_step(state, actions, reward)
     print(f"Mu = {mu}")
     print(f"Sigma = {sigma}")
     print(f"loss = {loss}")
     print(f"log_probabilities train= {log_probabilities}")
-
+    print(f"gradients train= {gradients}")
     pass
 
 